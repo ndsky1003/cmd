fe79d0227cb7a9be6218d803a2b21a0dbeebc48b
cc <dd@cc.com>
2025-11-15 19:50:38 +0700

    fix

--- structset/main.go
+++ structset/main.go
@@ -18,6 +18,17 @@ import (
 	"github.com/samber/lo"
 )
 
+const VERSION = "v1.0.1"
+
+const (
+	StructFieldAttr_inc     = "inc"
+	StructFieldAttr_no_inc  = "noinc"
+	StructFieldAttr_add     = "add"
+	StructFieldAttr_no_add  = "noadd"
+	StructFieldAttr_copy    = "copy"
+	StructFieldAttr_no_copy = "nocopy"
+)
+
 var inc_type_keys = []string{
 	"int",
 	"int8",
@@ -60,13 +71,21 @@ func shouldIgnoreField(tag string) bool {
 	return false
 }
 
-func getTagValue(tag, tag_name string) (tag_value string, qualifier []string) {
+func getTagValue(tag, tag_name string) (tag_value string, qualifier map[string]string) {
 	structTag := reflect.StructTag(tag)
 	tag_value = structTag.Get(tag_name)
 	tag_values := strings.Split(tag_value, ",")
 	tag_value, is := lo.First(tag_values)
+	qualifier = make(map[string]string)
 	if is {
-		qualifier = tag_values[1:]
+		qualifier_arr := tag_values[1:]
+		qualifier = lo.SliceToMap(qualifier_arr, func(item string) (string, string) {
+			parts := strings.SplitN(item, ":", 2)
+			if len(parts) == 2 {
+				return parts[0], parts[1]
+			}
+			return parts[0], ""
+		})
 	}
 	return
 }
@@ -80,24 +99,48 @@ func fix_default_tag_value(tag_value, default_value string) string {
 
 // 格式化 Go 文件
 func formatGoFile(filePath string) error {
-	cmd := exec.Command("gofmt", "-w", filePath)
+	cmd := exec.Command("goimports", "-w", filePath)
 	return cmd.Run()
 }
 
 func main() {
-	flag.String("useage", "useage", "structset -k=false -a=false -i=false --um=false -s=false --im=false --sub=ccc --tagname=structset")
+	flag.String("useage", "useage", "structset -k=false  --sub=ccc --tagname=structset")
+	attr := flag.Bool("attr", false, "显示可以可设置的属性")
+	version := flag.Bool("version", false, "显示版本号")
+	v := flag.Bool("v", false, "显示版本号")
 	flag.StringVar(&file_path, "f", "", "需要解析的文件")
 	flag.StringVar(&out_path, "o", "", "输出的新文件")
 	flag.StringVar(&suffix, "sub", "_struct_gen", "输出的新文件的后缀")
 	flag.StringVar(&tag_name, "tagname", "structset", "提取的tag名称.eg:json")
 	flag.BoolVar(&fileStructData.IsKey, "k", true, "是否生成结构体的keys常量")
 	flag.BoolVar(&fileStructData.IsJsonM, "jm", true, "是否生成结构体的bson.M")
-	flag.BoolVar(&fileStructData.IsUpM, "um", true, "是否生成结构体的bson.D,$set")
-	flag.BoolVar(&fileStructData.IsIncM, "im", true, "是否生成结构体的bson.D,$inc")
+	flag.BoolVar(&fileStructData.IsBsonM, "bm", true, "是否生成结构体的bson.M,$set")
+	flag.BoolVar(&fileStructData.IsIncM, "im", true, "是否生成结构体的bson.M,$inc")
+	flag.BoolVar(&fileStructData.IsBsonD, "bd", true, "是否生成结构体的bson.D,$set")
+	flag.BoolVar(&fileStructData.IsIncD, "id", true, "是否生成结构体的bson.D,$inc")
 	flag.BoolVar(&fileStructData.IsSet, "s", true, "是否生成结构体的Set(detal *StructData,keys []string)")
 	flag.BoolVar(&fileStructData.IsInc, "i", true, "是否生成结构体的Inc(detal *StructData,keys []string)")
 	flag.BoolVar(&fileStructData.IsAdd, "a", true, "是否生成结构体的Add(detal *StructData)")
+	flag.BoolVar(&fileStructData.IsCopy, "c", true, "是否生成结构体的Copy(keys []string)")
 	flag.Parse()
+	if *attr {
+		fmt.Println("attr:", []string{
+			StructFieldAttr_inc,
+			StructFieldAttr_no_inc,
+			StructFieldAttr_add,
+			StructFieldAttr_no_add,
+			StructFieldAttr_copy,
+			StructFieldAttr_no_copy})
+		fmt.Printf("可自由定制方法或者函数，函数以f_开头即可，其他都是方法\n")
+		fmt.Println("usage:structset:\"ccc,copy:f_cc,add:add_method\"")
+		return
+	}
+
+	if *v || *version {
+		fmt.Println(VERSION)
+		return
+	}
+
 	if file_path == "" {
 		dir, err := os.Getwd()
 		if err != nil {
@@ -140,29 +183,28 @@ func main() {
 				struct_name := typeSpec.Name.Name
 				fields := []Field{}
 
-				var is_have_number = false
+				var is_have_inc_key = false
+				var is_have_add_key = false
+				var is_have_copy_key = false
 				for _, field := range structType.Fields.List {
 					tag := getFieldTag(field)
 					if shouldIgnoreField(tag) {
 						continue
 					}
-					tag_value, _ := getTagValue(tag, tag_name)
+					tag_value, qualifiers := getTagValue(tag, tag_name)
 					json_tag_value, _ := getTagValue(tag, "json")
 					bson_tag_value, _ := getTagValue(tag, "bson")
-					// if idx := strings.Index(tag_name_value, ","); idx != -1 {
-					// 	tag_name_value = tag_name_value[:idx] // 只保留第一个逗号前的部分
-					// }
-					// if tag_value == "" {
-					// 	continue
-					// }
+					filedType := getFieldType(field.Type)
+
+					is_inc := lo.Contains(inc_type_keys, filedType)
+					is_inc, _, _ = get_tag_attr_is_method_func(qualifiers, is_inc, StructFieldAttr_inc, StructFieldAttr_no_inc)
+
+					is_add := lo.Contains(inc_type_keys, filedType)
+					is_add, add_f, add_m := get_tag_attr_is_method_func(qualifiers, is_add, StructFieldAttr_add, StructFieldAttr_no_add)
+
+					is_copy := true
+					is_copy, copy_f, copy_m := get_tag_attr_is_method_func(qualifiers, is_copy, StructFieldAttr_copy, StructFieldAttr_no_copy)
 					for _, name := range field.Names {
-						filedType := getFieldType(field.Type)
-						if tag_value == "" {
-							tag_value = name.Name // 默认使用字段名作为标签值
-						}
-						if json_tag_value == "" {
-							json_tag_value = name.Name // 默认使用字段名作为标签值
-						}
 						Name := name.Name
 						field := Field{
 							StructName:   struct_name,
@@ -171,18 +213,32 @@ func main() {
 							JsonTagValue: fix_default_tag_value(json_tag_value, Name),
 							BsonTagValue: fix_default_tag_value(bson_tag_value, Name),
 							Type:         filedType,
-							IsInc:        lo.Contains(inc_type_keys, filedType),
+							IsInc:        is_inc,
+							IsAdd:        is_add,
+							AddFunc:      add_f,
+							AddMethod:    add_m,
+							IsCopy:       is_copy,
+							CopyFunc:     copy_f,
+							CopyMethod:   copy_m,
 						}
 						if field.IsInc {
-							is_have_number = true
+							is_have_inc_key = true
+						}
+						if field.IsAdd {
+							is_have_add_key = true
+						}
+						if field.IsCopy {
+							is_have_copy_key = true
 						}
 						fields = append(fields, field)
 					}
 				}
 				structs = append(structs, StructData{
-					StructName:      struct_name,
-					IsHashNumberKey: is_have_number,
-					Fields:          fields,
+					StructName:    struct_name,
+					IsHaveIncKey:  is_have_inc_key,
+					IsHaveAddKey:  is_have_add_key,
+					IsHaveCopyKey: is_have_copy_key,
+					Fields:        fields,
 				})
 			}
 		}
@@ -210,13 +266,6 @@ func main() {
 		panic(err)
 	}
 
-	// for _, s := range structs {
-	// 	fmt.Printf("Struct: %s\n", s.StructName)
-	// 	for _, field := range s.Fields {
-	// 		fmt.Printf("  Field: %s, Type: %s,Inc:%v\n", field.Name, field.Type, field.IsInc)
-	// 	}
-	// }
-
 }
 
 type Field struct {
@@ -228,12 +277,45 @@ type Field struct {
 	Type         string
 	Tag          string // 新增字段用于存储标签信息
 	IsInc        bool
+	IsAdd        bool
+	IsCopy       bool
+	AddMethod    string
+	CopyMethod   string //m 是方法，属于结构体。f_是函数，属于包
+	AddFunc      string
+	CopyFunc     string //m 是方法，属于结构体。f_是函数，属于包
 }
 
 type StructData struct {
-	StructName      string
-	IsHashNumberKey bool
-	Fields          []Field
+	StructName    string
+	IsHaveIncKey  bool
+	IsHaveAddKey  bool
+	IsHaveCopyKey bool
+	Fields        []Field
+}
+
+func get_tag_attr_is_method_func(attrs map[string]string, default_attr_value bool, attr, no_attr string) (is bool, func_, method string) {
+	is = default_attr_value
+
+	if _, ok := attrs[no_attr]; ok {
+		is = false
+		return
+	}
+
+	if m_or_f, ok := attrs[attr]; ok {
+		if m_or_f != "" {
+			if strings.HasPrefix(m_or_f, "f_") {
+				func_ = strings.TrimLeft(m_or_f, "f_")
+			} else {
+				if strings.HasPrefix(m_or_f, "m_") {
+					method = strings.TrimLeft(m_or_f, "m_")
+				} else {
+					method = m_or_f
+				}
+			}
+		}
+		is = true
+	}
+	return
 }
 
 type FileStructData struct {
@@ -241,11 +323,14 @@ type FileStructData struct {
 	StructDatas []StructData
 	IsKey       bool
 	IsJsonM     bool
-	IsUpM       bool
-	IsIncM      bool
+	IsBsonM     bool //bson.M
+	IsIncM      bool //bson.M
+	IsBsonD     bool //bson.D
+	IsIncD      bool //bson.D
 	IsSet       bool
 	IsInc       bool
 	IsAdd       bool
+	IsCopy      bool
 }
 
 // func getStructName(expr ast.Expr) string {
